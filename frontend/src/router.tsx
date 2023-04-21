ee6dd12e43c379667c49f8d2f2e886d5e7aa8045
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-04-21 18:12:04 +0200

    Put all pages inside the main app container

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,14 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
 import Home from "./pages/Home";
 import Match from "./pages/Match";
+import WhoAreYou from "./pages/WhoAreYou";
 
 export default createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/:id",
-    element: <Match />,
-  },
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+
+            {
+                path: "/:matchName",
+                element: <Match />,
+            },
+            {
+                path: "/whoru",
+                element: <WhoAreYou />,
+            },
+        ]
+    }
 ]);
