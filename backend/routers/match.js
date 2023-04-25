2575216ff45c1e90a1d25bc55f29d090bde0f673
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-04-25 22:01:48 +0200

    feat: Replace match endpoints with useful ones
    All that's needed client-side is a way to create a match
    in the database, and a way to get a matches data.
    
    Joining a match is now handled using a socket message instead.

--- backend/routers/match.js
+++ backend/routers/match.js
@@ -1,52 +1,56 @@
 import { Router } from "express";
 import Match from "../models/match.js";
+import User from "../models/user.js";
 
 const router = Router();
 
-// Create a new match
+router.get("/:matchName", async (req, res) => {
+    const { matchName } = req.params;
+
+    const match = await Match.findOne({ name: matchName });
+
+    if (!match) {
+        return res.status(404).json({ error: "Match not found" });
+    }
+
+    const organizer = await User.findOne({ _id: match.organizer });
+    const players = match.players.map(async (player) => {
+        const user = await User.findOne({ _id: player._id });
+        return {
+            username: user.username,
+            _id: user._id,
+            score: player.score,
+        };
+    });
+
+    res.status(200).json({
+        _id: match._id,
+        name: match.name,
+        organizer: {
+            username: organizer.username,
+            _id: organizer._id,
+        },
+        players: await Promise.all(players),
+        status: match.status
+    });
+});
+
 router.post("/", async (req, res) => {
-    // To create a match, we need to know the name of the match, the name of the organizer.
-    // Every match starts with no players.
-    const { name, organizer } = req.body;
+    const { matchName, organizer } = req.body;
+
+    const user = await User.findOne({ username: organizer });
+
     const match = new Match({
-        name,
-        organizer: { name: organizer },
+        name: matchName,
+        organizer: user._id,
     });
 
     try {
         await match.save();
-        res.status(201).json(match);
+        res.status(201);
     } catch (error) {
         if (error.code == 11000) {
             res.status(400).json({ error: "Match name already exists" });
-        } else {
-            res.status(500).json({ error: "Something went wrong" });
-        }
-    }
-});
-
-// Join an existing match
-router.post("/:matchId", async (req, res) => {
-    // To join a match, we need to know the ID of the match, and the name of the player.
-    const { matchId } = req.params;
-    const { name } = req.body;
-
-    try {
-        const match = await findById(matchId);
-        if (!match) {
-            return res.status(404).json({ error: "Match not found" });
-        }
-
-        if (match.players.find((player) => player.name === name)) {
-            return res.status(400).json({ error: "Player already exists" });
-        }
-
-        match.players.push({ name });
-        await match.save();
-        res.json(match);
-    } catch (error) {
-        if (error.name === "CastError") {
-            return res.status(400).json({ error: "Invalid match ID" });
         } else {
             res.status(500).json({ error: "Something went wrong" });
         }
