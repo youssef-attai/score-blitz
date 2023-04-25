2c6ac1dd6c4213e8b9f9d200af8480c01735a147
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-04-25 21:16:23 +0200

    clean up: Remove debugging logs

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -6,22 +6,17 @@ const router = Router();
 
 router.get("/me", async (req, res) => {
     const { token } = req.cookies;
-    console.log("token:", token);
 
     if (!token) {
-        console.log("No token is found", token);
         return res.status(401).json({ message: "Unauthorized" });
     }
 
     const user = await User.findOne({ token });
 
-    // There might be a token in the cookie, but it might not be valid
     if (!user) {
-        console.log("Invalid token", token);
         return res.status(401).json({ message: "Unauthorized" });
     }
 
-    console.log("User found:", user);
     return res.status(200).json({
         username: user.username,
     });
@@ -30,20 +25,13 @@ router.get("/me", async (req, res) => {
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
 
-    console.log("username:", username);
-    console.log("password:", password);
-
     const user = await User.findOne({ username, password });
 
-    console.log("user:", user);
 
     if (user) {
-        console.log("User found:", user);
-
         user.token = randomBytes(64).toString("hex");
         await user.save();
 
-        console.log("User updated:", user);
 
         res.cookie("token", user.token, {
             httpOnly: true,
@@ -55,7 +43,6 @@ router.post("/login", async (req, res) => {
         });
     }
 
-    console.log("Invalid credentials");
 
     return res.status(401).json({ message: "Invalid credentials" });
 });
@@ -63,14 +50,10 @@ router.post("/login", async (req, res) => {
 router.post("/create", async (req, res) => {
     const { username, password } = req.body;
 
-    console.log("username:", username);
-    console.log("password:", password);
-
     const newUser = new User({ username, password });
     newUser.token = randomBytes(64).toString("hex");
     await newUser.save();
 
-    console.log("newUser:", newUser);
 
     res.cookie("token", newUser.token, {
         httpOnly: true,
@@ -84,8 +67,6 @@ router.post("/create", async (req, res) => {
 
 router.get('/logout', (_, res) => {
     res.clearCookie('token');
-
-    console.log('User logged out');
 
     return res.sendStatus(200);
 });
