581f420d902a8b880d7341ccef9c10a5506b770d
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-04-25 22:08:58 +0200

    feat: Create the match model client-side

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -3,3 +3,19 @@ export interface User {
     username: string;
     token: string
 }
+
+export interface Match {
+    _id: string;
+    name: string;
+    organizer: {
+        username: string;
+        _id: string;
+    };
+    players: {
+        username: string;
+        _id: string;
+        score: number;
+    }[];
+    status: "new" | "playing" | "ended";
+}
+
